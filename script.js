155d5dbcc4606afff6610be395890fa150f5775b
Badhon Roy <badhonraybd@gmail.com>
2026-07-10 03:00:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Welcome");
+const roles=["Mathematics Student","Web Enthusiast","Open Source Learner"];
+let i=0,j=0,d=1,e=document.getElementById("typing");
+setInterval(()=>{
+e.textContent=roles[i].slice(0,j);
+j+=d;
+if(j>roles[i].length)d=-1;
+if(j<0){d=1;i=(i+1)%roles.length;j=0;}
+},120);
